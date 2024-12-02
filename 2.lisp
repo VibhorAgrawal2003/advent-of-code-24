ee86c9a97bd814df12055fb16f5e5afdce2b7cfe
Vibhor <vibhorag2003@gmail.com>
2024-12-02 22:54:41 +0530

    add: day 2 part 2

--- 2.lisp
+++ 2.lisp
@@ -1,18 +1,24 @@
 ;; Function to read input and count safe seq
 (defun read-file-into-array (file-path)
   (with-open-file (stream file-path :direction :input)
-    (let ((cnt 0) (A '()))
+    (let ((cnt 0) (newcnt 0) (A '()))
       (loop for line = (read-line stream nil)
         while line
         do
         (let ((numbers (mapcar #'parse-integer (split-sequence #\Space line))))
           (setq A (apply #'vector numbers))
-          (when (validate A)
-            (print A)
-            (incf cnt))))
-    (format t "~%~%Total safe sequences: ~d ~%" cnt)
-    )
-  )
+          (let ((safe (validate A)))
+            (when safe
+              (incf cnt))
+            (when (not safe)
+              (when (problem-dampener A)
+                (incf newcnt)
+              )
+            )
+          )))
+    (format t "Total safe sequences: ~d ~%" cnt)
+    (format t "Total safe sequences (with dampener): ~d ~%" (+ newcnt cnt))
+    ))
 )
 
 ;; Function to split a string by a single space
@@ -23,8 +29,28 @@
           do (push (subseq string start end) tokens)
           while end
           finally (push (subseq string start) tokens))
-    (butlast (nreverse tokens))
-  ))
+    (butlast (nreverse tokens)))
+)
+
+
+;; Function to remove an item from array
+(defun remove-element-at-index (array index)
+  (let ((new-array (make-array (1- (length array)))))
+    (loop for i from 0 to (1- (length array)) do
+      (unless (= i index)
+        (setf (aref new-array (if (< i index) i (1- i)))
+              (aref array i))))
+    new-array)
+)
+
+
+;; Function to copy an array
+(defun copy-array (arr)
+  (let ((new-arr (make-array (array-dimension arr 0))))
+    (dotimes (i (length arr))
+      (setf (aref new-arr i) (aref arr i)))
+    new-arr)
+)
 
 
 ;; Function to validate a report A by inc/dec order
@@ -41,9 +67,26 @@
         (if (not (and (> x y) (<= (- x y) 3)))
           (setq decreasing-flag nil))))
 
-    (or increasing-flag decreasing-flag)))
+    (or increasing-flag decreasing-flag))
+)
 
-    
+
+;; Function to dampen a report A that was unsafe
+(defun problem-dampener (A)
+  (let ((safe nil))
+  
+    (dotimes (i (length A))
+      (let ((B (copy-array A)))
+        (setq B (remove-element-at-index B i))
+        (if (validate B)
+          (setq safe t)
+        )
+      )
+    )
+    safe)
+)
+
+
 ;; Call to the main function
 (read-file-into-array "inputs/2.txt")
 
