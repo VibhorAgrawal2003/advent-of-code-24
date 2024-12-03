b0d7c0eadeb26674d18ad5611b46dbe781101bab
Vibhor <vibhorag2003@gmail.com>
2024-12-04 03:39:25 +0530

    add: day 3 part 1

---
+++ 3.lisp
@@ -0,0 +1,102 @@
+;; Read file input into a string and return
+(defun read-file-and-process (file-path)
+  (let ((content ""))
+    (with-open-file (stream file-path :direction :input)
+      (loop for line = (read-line stream nil nil)
+        while line
+        do (setf content (concatenate 'string content line))))
+  content))
+
+
+;; Calculating result from input string
+(defun calculate-result (content)
+  (let ((buffer "") (n (length content)) (result 0) (a 0) (b 0) (s 0) (r 0))
+
+    ;; Traverse entire string
+    (loop for i from 0 below n
+      do
+      (setf buffer (concatenate 'string buffer (string (char content i))))
+
+      ;; Look for a match
+      (when (search "mul(" buffer)
+        (setf buffer "")
+
+        ;; Extract first value or keep it zero
+        (let ((num 0))
+          (loop for j from (+ i 1) below n
+            do
+
+            ;; Sequence for obtaining first value
+            (if (digit-char-p (char content j))
+              (progn ;then
+              (incf s)
+              (setf num (* num 10))
+              (setf num (+ num (digit-char-p (char content j))))
+              )
+
+              (progn ;else
+                (if (char= #\, (char content j))
+                  (progn
+                  (setf a num)
+                  (setf num 0)
+                  (return)
+                  )
+                  (return)
+                )
+              )
+            )
+            ;;end of loop
+          )
+        )
+
+        ;; Extract second value or keep it zero
+        (let ((num 0))
+          (loop for j from (+ i s 2) below n
+            do
+
+            ;; Sequence for obtaining first value
+            (if (digit-char-p (char content j))
+              (progn ;then
+              (incf r)
+              (setf num (* num 10))
+              (setf num (+ num (digit-char-p (char content j))))
+              )
+
+              (progn ;else
+                (if (= (char-code (char content j)) 41)
+                  (progn
+                  (setf b num)
+                  (setf num 0)
+                  (return)
+                  )
+                  (return)
+                )
+              )
+            )
+            ;;end of loop
+          )
+        )
+        (when (and (> a 0) (> b 0) (> s 0) (> r 0) (< s 4) (< r 4))
+          (format t "~d ~d ~%" a b)
+          (setq result (+ result (* a b)))
+        )
+        (setq s 0)
+        (setq r 0)
+        (setq a 0)
+        (setq b 0)
+
+        ;;end of match
+      )
+    )
+
+    (format t "Final Result: ~d ~%" result)
+  )
+)
+
+
+
+;; Driver code
+(let ((content (read-file-and-process "inputs/3.txt")))
+  (calculate-result content)
+)
+
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Collection of solutions to Advent of Code 2024, written in Common Lisp.
 <a href="https://andreyor.st/posts/2020-12-03-we-need-to-talk-about-parentheses/"><img src="images/lisp.jpg"></a>
 <br />
 
-* Historian Hysteria: [question](https://adventofcode.com/2024/day/1) && [solution](1.lisp)
-* Red-Nosed Reports: [question](https://adventofcode.com/2024/day/2) && [solution](2.lisp)
+* Historian Hysteria: (and [question](https://adventofcode.com/2024/day/1) [solution](1.lisp)) 
+* Red-Nosed Reports: (and [question](https://adventofcode.com/2024/day/2) [solution](2.lisp)) 
+* Mull It Over: (and [question](https://adventofcode.com/2024/day/3) [solution](3.lisp))
